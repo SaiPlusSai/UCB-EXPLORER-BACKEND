da0f90f696733b4867cdded71a4b4ce0e884409d
SaiPlusSai <refgu774@gmail.com>
2026-05-11 00:03:05 -0400

    cambios back
    cambios back

--- src/app.js
+++ src/app.js
@@ -6,14 +6,29 @@ const errorHandler = require("../middleware/errorHandler");
 
 const app = express();
 
-app.use(cors({ origin: true, credentials: true }));
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: [
+    "Origin",
+    "X-Requested-With",
+    "Content-Type",
+    "Accept",
+    "Authorization",
+    "ngrok-skip-browser-warning"
+  ],
+}));
+
 app.use(express.json({ limit: "2mb" }));
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api", routes);
 
 app.use((req, res) => {
-  res.status(404).json({ ok: false, error: "Recurso no encontrado" });
+  res.status(404).json({
+    ok: false,
+    error: "Recurso no encontrado"
+  });
 });
 
 app.use(errorHandler);
